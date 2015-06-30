5d58847b28c710fbb51272476d43e2ee10bec9e9
yuhi TOMARI <tomari_yuhi@cyberagent.co.jp>
2015-06-30 16:09:29 +0900

    hello sample

--- Hello/script/app.js
+++ Hello/script/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module("myApp",[]);
 // controller
 myApp.controller("HelloController", function($scope){
   // model
-  $scope.explain = "AngularはフロントサイドMVCフレームワークです。";
+  $scope.explain = "Event handling1";
   $scope.word = "Hello World";
   $scope.action = function(){
     window.open();
@@ -12,11 +12,11 @@ myApp.controller("HelloController", function($scope){
 
 // controller
 myApp.controller("ByeByeController", function($scope){
-  $scope.explain = "コントローラはスコープ内のモデルを参照します。";
+  $scope.explain = "Event handling2";
   var cur = 0;
   var text = [];
-  text[0] ="Bye Bye";
-  text[1] ="Thank you";
+  text[0] ="Hey!";
+  text[1] ="World!";
   
   $scope.word = text[cur];
   $scope.action = function(){
@@ -28,7 +28,7 @@ myApp.controller("ByeByeController", function($scope){
 // controller
 myApp.controller("RepeatController", function($scope){
   // model
-  $scope.explain = "ビューとモデルは自動的に双方向で値をバインドします。";
+  $scope.explain = "Data Binding";
   $scope.word = "";
   $scope.action = function(){
     alert($scope.word);
